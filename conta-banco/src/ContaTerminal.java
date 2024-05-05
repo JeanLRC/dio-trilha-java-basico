6a8b50d64dd6a86cfd1bafb9f9c6b21b6cfd39c2
JeanLRC <jeanlucas588@outlook.com>
2024-05-05 14:15:09 -0300

    feat: complete syntax challenge

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeDoCliente;
+        double saldo;
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        System.out.println("Por favor, digite o número da Agência!");
+        numero = sc.nextInt();
+
+        System.out.println("Por favor, insira a Agência!");
+        sc.nextLine();
+        agencia = sc.nextLine();
+        
+        System.out.println("Por favor, insira seu nome!");
+        nomeDoCliente = sc.nextLine();
+        
+        System.out.println("Por favor, insira seu saldo!");
+        saldo = sc.nextDouble();
+
+        String mensagem = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponivel para saque.", nomeDoCliente, agencia, numero, saldo);
+
+        System.out.println(mensagem);
+        sc.close();
     }
 }
